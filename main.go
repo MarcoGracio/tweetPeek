64cf9c5524be9490cec468ad474a2cbcdac1bc86
Marco Grácio <marco.gracio@farfetch.com>
2024-01-02 11:16:59 +0000

    feat: add new page to test

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 
 func main() {
 	retryStrategy, _ := calculateBestStrategy(rand.Intn(20))
-	fmt.Println("Attempt nr:" + strconv.Itoa(retryStrategy.GetCurrentAttempt()))
 	strResp := retryStrategy.Apply(requestProcessor)
-	fmt.Println("Attempt nr:" + strconv.Itoa(retryStrategy.GetCurrentAttempt()))
-	fmt.Println(strResp)
+	fmt.Printf("Attempt nr: %v \n%v", strconv.Itoa(retryStrategy.GetCurrentAttempt()), strResp)
+
+	retryStrategy, _ = calculateBestStrategy(rand.Intn(20))
+	strResp = retryStrategy.Apply(func(currentAttempt int) (*http.Response, error) {
+		return http.Get("http://google.com/")
+	})
+	fmt.Printf("Attempt nr: %v \n%v", strconv.Itoa(retryStrategy.GetCurrentAttempt()), strResp)
+
 }
 
 func calculateBestStrategy(random int) (strategy.IStrategy, error) {
